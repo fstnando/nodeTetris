221f6afdd70b181051d613985cfd00b57bee1d0e
Sergio Fernando Sanz Troiani <fstnando@gmail.com>
2015-07-22 12:45:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -136,6 +136,6 @@ function main_loop() {
 
 process.nextTick(main_loop);
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(8080, function(){
+    console.log('listening on *:8080');
 });

0a175e1a8348a18cbc3768c9c0adad53d4577347
Sergio Fernando Sanz Troiani <fstnando@gmail.com>
2015-07-22 16:19:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -140,5 +140,5 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 http.listen(server_port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", server_port " + port )
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });

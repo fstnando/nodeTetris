4f2bb89cbe214e7ae86b0fc0e93fb723d21648db
fstnando <fstnando@gmail.com>
2015-07-31 08:02:20 -0300

    Touch Event

--- public/js/mouse.js
+++ public/js/mouse.js
@@ -21,16 +21,38 @@ $(document).mouseup(function(e){
     Mouse.ly = e.pageY - Mouse.ly;
     Mouse.update();
 });
-$(window).on("touchstart", function(ev) {
-    Mouse.lx = e.pageX;
-    Mouse.ly = e.pageY;
+/*
+document.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    var touch = e.touches[0];
+    Mouse.lx = touch.pageX;
+    Mouse.ly = touch.pageY;
 });
-$(window).on("touchmove", function(ev) {
-    Mouse.px = e.pageX;
-    Mouse.py = e.pageY;
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+    var touch = e.touches[0];
+    Mouse.px = touch.pageX;
+    Mouse.py = touch.pageY;
 });
-$(window).on("touchend", function(ev) {
-    Mouse.lx = e.pageX - Mouse.lx;
-    Mouse.ly = e.pageY - Mouse.ly;
+document.addEventListener('touchend', function(e) {
+    e.preventDefault();
+    var touch = e.touches[0];
+    Mouse.lx = touch.pageX - Mouse.lx;
+    Mouse.ly = touch.pageY - Mouse.ly;
     Mouse.update();
+});
+*/
+$(document).swipe( {
+    swipeLeft: function(event, direction, distance, duration, fingerCount){
+        Mouse.lx = -distance;
+    }
+    swipeRight: function(event, direction, distance, duration, fingerCount){
+        Mouse.lx = distance;
+    }
+    swipeUp: function(event, direction, distance, duration, fingerCount){
+        Mouse.ly = -distance;
+    }
+    swipeDown: function(event, direction, distance, duration, fingerCount){
+        Mouse.ly = distance;
+    }
 });
